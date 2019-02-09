c9ded9b7acd64e65d9644e9575c7339531d8d195
Tony Braasch <tonybraasch@gmail.com>
2019-02-09 14:59:35 -0600

    add functionality for base mode calculator

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,73 @@
 $(document).ready(onReady);
 
+let calcMethod;
+
 function onReady() {
     console.log('jquery is running');
 
+    // get the current content from 'calculations' array of objects on the server and append to DOM
+    getCalculation();
+
+    // will store the current calculation method to varialbe 'calcMethod' based on button clicked on DOM
+    $('.calc-btn').on('click', function () {
+        console.log('calc method is:', $(this).text());
+        calcMethod = $(this).text();
+        $(this).css('background-color', '#5c918c');
+    });
+
+    // will run upon click of the equals button on DOM
+    $('#equals-submit-btn').on('click', submitCalculation);
+
+    // clear user input fields when 'C' button is clicked
+    $('#clear-btn').on('click', function () {
+        $('#calc-input-row input').val('');
+        $('button').css('background-color', 'white');
+    })
+}
+
+
+
+// will send to server the inputs from calculator on DOM
+function submitCalculation() {
+    console.log('equals/submit button clicked');
+
+    $.ajax({
+        method: 'POST',
+        url: '/post-calculation',
+        data: {
+            inputFirst: $('#number-first-input').val(),
+            calcMethod: calcMethod,
+            inputSecond: $('#number-second-input').val()
+        }
+    }).then(function () {
+        // get the current content from 'calculations' array of objects on the server and append to DOM
+        getCalculation();
+    })
+
+    $('#calc-input-row input').val('');
+    $('button').css('background-color', 'white');
+}
+
+
+
+// get the current content from 'calculations' array of objects on the server and append to DOM
+function getCalculation() {
+    $.ajax({
+        method: 'GET',
+        url: '/get-calculation'
+    }).then(function (response) {
+
+        $('#calc-output-list').empty();
+        console.log(response);
+
+        response.forEach(function (calculations) {
+            $('#calc-output-number').empty();
+            $('#calc-output-number').append(`${calculations.calcTotal}
+        `)
+            $('#calc-output-list').append(`
+        <li>${calculations.inputFirst} ${calculations.calcMethod} ${calculations.inputSecond} &#x0003D ${calculations.calcTotal} 
+        `);
+
+        });
+    })
 }
--- server/server.js
+++ server/server.js
@@ -10,3 +10,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
+
+
+
+// array of objects that will hold the calculations
+let calculations = [];
+
+
+// recieve the inputs of the calculator on the DOM
+app.post('/post-calculation', (req, res) => {
+    let calcInput = req.body;
+    console.log(calcInput);
+    calcInput.calcTotal = executeCalculation(calcInput);
+    calculations.push(calcInput);
+    res.sendStatus(201);
+});
+
+
+// send the current contents of the 'calculations' array of objects
+app.get('/get-calculation', (req, res) => {
+    res.send(calculations);
+});
+
+
+// calculate the total of what was inputed and submitted from calculator on DOM
+function executeCalculation(calcInput) {
+    calcInput.inputFirst = Number(calcInput.inputFirst);
+    calcInput.inputSecond = Number(calcInput.inputSecond);
+
+    if (calcInput.calcMethod == '+') {
+        return calcInput.inputFirst + calcInput.inputSecond;
+    }
+    if (encodeURI(calcInput.calcMethod) == '%E2%88%92') {
+        return calcInput.inputFirst - calcInput.inputSecond;
+    }
+    if (encodeURI(calcInput.calcMethod) == '%C3%97') {
+        return calcInput.inputFirst * calcInput.inputSecond;
+    }
+    if (encodeURI(calcInput.calcMethod) == "%C3%B7") {
+        return calcInput.inputFirst / calcInput.inputSecond;
+    }
+}
